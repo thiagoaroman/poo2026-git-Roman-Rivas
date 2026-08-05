7d15fe8d4a730459f5174dd71ad9ff3bc690e3ff
Thiago Roman <romanthiagoandres@gmail.com>
2026-08-05 18:15:54 -0300

    Personaliza el saludo de A

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         // Paso 4: cada integrante modifica ESTA misma línea con su propio saludo.
-        System.out.println("Hola Mundo");
+        System.out.println("Hola soy Thiago y esto es POO 2026");
     }
 }
