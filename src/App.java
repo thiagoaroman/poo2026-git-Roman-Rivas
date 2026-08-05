c83f397213ac7f69d794f30a25308e4f4280f22a
Gaston Rivas <fabriicio.808@gmail.com>
2026-08-05 18:16:24 -0300

    sixseven

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         // Paso 4: cada integrante modifica ESTA misma línea con su propio saludo.
-        System.out.println("Hola Mundo");
+        System.out.println("hola soy fabri");
     }
 }
